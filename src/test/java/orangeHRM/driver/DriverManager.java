6a112d0ccf2d1bd39b721e00d627dd6b70f11704
Oyne <andreoyy.litvinovv@gmail.com>
2026-03-29 12:00:21 +0300

    Manager updated to use Garcia DriverManager

--- src/test/java/orangeHRM/driver/DriverManager.java
+++ src/test/java/orangeHRM/driver/DriverManager.java
@@ -1,35 +1,52 @@
 package orangeHRM.driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.aeonbits.owner.ConfigFactory;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
 
 public class DriverManager {
 
     private static final ThreadLocal<WebDriver> driver = new ThreadLocal<>();
     private static final DriverConfig cfg = ConfigFactory.create(DriverConfig.class);
 
-    public static synchronized WebDriver getDriver() {
+    public static WebDriver getDriver() {
         if (driver.get() == null) {
+            WebDriverManager.chromedriver().setup();
+
             ChromeOptions options = new ChromeOptions();
 
-            options.addArguments("--remote-allow-origins=" + cfg.remoteOrigins());
-            if (cfg.maximize()) options.addArguments("--start-maximized");
-            if (cfg.headless()) options.addArguments("--headless=new");
+            if (!cfg.remoteOrigins().isEmpty()) {
+                options.addArguments("--remote-allow-origins=" + cfg.remoteOrigins());
+            }
 
-           /* System.setProperty("webdriver.chrome.driver",
-                    System.getProperty("user.dir") + "/" + cfg.driverPath());*/
+            if (cfg.maximize()) {
+                options.addArguments("--start-maximized");
+            }
 
-            driver.set(new ChromeDriver(options));
+            if (cfg.headless()) {
+                options.addArguments("--headless=new");
+            }
+
+            WebDriver webDriver = new ChromeDriver(options);
+
+            webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+            driver.set(webDriver);
+
             driver.get().get(cfg.baseUrl());
         }
         return driver.get();
     }
 
     public static void quitDriver() {
-        if (driver.get() != null) {
-            driver.get().quit();
+        WebDriver webDriver = driver.get();
+
+        if (webDriver != null) {
+            webDriver.quit();
             driver.remove();
         }
     }
